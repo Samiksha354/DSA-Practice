915e561a4fda042aa2bd2bafc67913837030034f
samiksha jadhav <samikshajadhav354@gmail.com>
2026-02-05 14:05:58 +0530

    Added Java DSA practice programs

---
+++ src/Adding_One.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Adding_One {
+    public static int adding_onee(int[] arr){
+        return 0;
+
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter the siz of the array:");
+        int n=sc.nextInt();
+
+        int[] arr=new int[n];
+
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+}
